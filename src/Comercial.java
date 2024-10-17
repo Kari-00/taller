04a577af2e0e901b47938d7a0e7521d8934d3894
kari-00 <anatacha998@gmail.com>
2024-10-16 21:47:02 -0500

    Añadir clases Empleado y Comercial con validaciones y métodos adicionales

---
+++ src/Comercial.java
@@ -0,0 +1,77 @@
+import java.util.Objects;
+
+public class Comercial extends Empleado {
+
+    private double comision;
+
+    @Override
+    public boolean plus(double sueldoPlus) {
+        boolean aumento = false;
+        if (comision < 50) {
+            salario += sueldoPlus;
+            aumento = true;
+        }
+        return aumento;
+    }
+
+    public Comercial() {
+        super();
+        this.comision = 0;
+    }
+
+    public Comercial(String nombre, String apellido, int edad, double salario, double comision) {
+        super(nombre, apellido, edad, salario);
+        this.setComision(comision);  // Validación de comisión
+    }
+
+    public double getComision() {
+        return comision;
+    }
+
+    public void setComision(double comision) {
+        if (comision < 0) {
+            throw new IllegalArgumentException("La comisión no puede ser negativa.");
+        }
+        this.comision = comision;
+    }
+
+    public double calcularSalarioFinal() {
+        return this.salario + this.comision;
+    }
+
+    public void incrementarComision(double incremento) {
+        if (incremento > 0) {
+            this.comision += incremento;
+        } else {
+            throw new IllegalArgumentException("El incremento debe ser positivo.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Comercial{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
+                ", salario=" + salario +
+                ", comision=" + comision +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comercial comercial = (Comercial) o;
+        return Double.compare(comercial.comision, comision) == 0 &&
+                Double.compare(comercial.salario, salario) == 0 &&
+                edad == comercial.edad &&
+                Objects.equals(nombre, comercial.nombre) &&
+                Objects.equals(apellido, comercial.apellido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, apellido, edad, salario, comision);
+    }
+}
---
+++ src/Empleado.java
@@ -0,0 +1,88 @@
+import java.util.Objects;
+
+public abstract class Empleado {
+
+    protected final static double SALARIO_DEF = 600;
+
+    protected String nombre;
+    protected String apellido;
+    protected int edad;
+    protected double salario;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        if (edad < 18) {
+            throw new IllegalArgumentException("El empleado debe tener al menos 18 años.");
+        }
+        this.edad = edad;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if (salario < 0) {
+            throw new IllegalArgumentException("El salario no puede ser negativo.");
+        }
+        this.salario = salario;
+    }
+
+    public abstract boolean plus(double sueldoPlus);
+
+    public Empleado() {
+        this("", "", 0, SALARIO_DEF);
+    }
+
+    public Empleado(String nombre, String apellido, int edad, double salario) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.setEdad(edad);  // Validación de edad
+        this.setSalario(salario);  // Validación de salario
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
+                ", salario=" + salario +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Empleado empleado = (Empleado) o;
+        return edad == empleado.edad &&
+                Double.compare(empleado.salario, salario) == 0 &&
+                Objects.equals(nombre, empleado.nombre) &&
+                Objects.equals(apellido, empleado.apellido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, apellido, edad, salario);
+    }
+}
